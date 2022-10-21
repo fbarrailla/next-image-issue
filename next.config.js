77d6a43fdfc60c42c7a09965e97dace81c99a9a2
François Barrailla <francois.barrailla@gmail.com>
2022-10-21 12:06:49 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/test',
+  // comment the line below to and the image will display correctly
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["images.unsplash.com"]
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/404.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link"
+
+export default function Page404() {
+  return <Link href="/"><a>Follow the link 🐇</a></Link>
+}
